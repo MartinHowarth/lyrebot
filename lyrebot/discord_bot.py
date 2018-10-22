d791546f611390e9d7405ed1a20ebb5f1cad12bf
MartinHowarth <howarth.martin@gmail.com>
2018-10-22 21:30:09 +0100

    remove a log

--- lyrebot/discord_bot.py
+++ lyrebot/discord_bot.py
@@ -267,7 +267,6 @@ def create_bot(lyre_client_id, lyre_client_secret, lyre_redirect_uri):
         log.debug("tokens.yaml exists")
         with open(".tokens.yaml") as fi:
             token_dict = yaml.safe_load(fi)
-            log.debug("file tokens are: %s", token_dict)
             for user, token in token_dict.items():
                 log.info("loaded token from file for: %s", user)
                 lyrebot.lyrebird_tokens[user] = token
